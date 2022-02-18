2d1b96869b83f9fa79c40cbfb8f866fde5b1017b
thebonafidekfc <96156996+thebonafidekfc@users.noreply.github.com>
2022-02-17 20:13:43 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,9 +36,9 @@ sheet_name = "EoS Pinecone Mimi Test"
 @client.event
 async def on_ready():
     global cb_start
-    cb_start = get_cb_start_datetime(1, 13, 2022)
+    cb_start = get_cb_start_datetime(2, 18, 2022)
     print("The bot is ready!")
-worksheet_name = "CB12"
+worksheet_name = "CB13"
 
 ### END PART ###
 
